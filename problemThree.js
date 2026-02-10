68f8eb19487157b9042c1a9c34bc9475d2e87ad1
codexmish <majharulislam.shanto.70@gmail.com>
2026-02-10 17:09:13 +0600

    problem Three done

---
+++ problemThree.js
@@ -0,0 +1,18 @@
+function finalScore(omr) {
+  const { right, wrong, skip } = omr;
+
+  if (
+    typeof right !== "number" &&
+    typeof wrong !== "number" &&
+    typeof skip !== "number"
+  ) {
+    return "Invalid";
+  }
+
+  if (right + wrong + skip !== 100) {
+    return "Invalid";
+  }
+
+  const score = right * 1 - wrong * 0.5;
+  return Math.round(score);
+}
